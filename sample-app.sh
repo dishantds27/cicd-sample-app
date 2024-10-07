2520d650316229dc1ed101dd241b2fc295d0937e
dishantds27 <dishant.saini@student.hogent.be>
2024-10-07 14:49:04 +0000

    in sample-app.sh /vagrant weggedaan voor het pad v sample-app.py

--- sample-app.sh
+++ sample-app.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 #mkdir tempdir/static
 
 #cp sample_app.py tempdir/.
-cp /vagrant/cicd-sample-app/sample-app.py tempdir/.
+cp /cicd-sample-app/sample-app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
